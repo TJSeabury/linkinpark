67bed9c3474519e7479cedc34e65bab2ddb0a1df
TJSeabury <tylerseabury@gmail.com>
2022-04-06 15:12:09 -0400

    exporting struct props and attempting channels

--- job.go
+++ job.go
@@ -1,38 +1,40 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gocolly/colly"
 	"github.com/google/uuid"
 )
 
 type job struct {
-	uuid         string
-	status       string
-	domain       string
-	linksFound   int
-	linksCrawled int
-	data         map[string]pageInfo
+	Uuid          string
+	Status        string
+	StatusChannel chan string
+	Domain        string
+	LinksFound    int
+	LinksCrawled  int
+	Data          map[string]pageInfo
+	DataChannel   chan map[string]pageInfo
 }
 
 func NewJob(domain string) job {
 	id := uuid.New()
 	return job{
-		uuid:   "job_" + id.String(),
-		status: "starting",
-		domain: domain,
+		Uuid:          "job_" + id.String(),
+		Status:        "starting",
+		StatusChannel: make(chan string),
+		Domain:        domain,
+		DataChannel:   make(chan map[string]pageInfo),
 	}
 }
 
-func (j *job) updateStatus(status string) {
-	j.status = status
-}
-
 func (j *job) addLinksFound(n int) {
-	j.linksFound += n
+	j.LinksFound += n
 }
 
 func (j *job) addLinksCrawled(n int) {
-	j.linksCrawled += n
+	j.LinksCrawled += n
 }
 
 func (j *job) crawl() {
@@ -41,7 +43,7 @@ func (j *job) crawl() {
 		// MaxDepth is 2, so only the links on the scraped page
 		// and links on those pages are visited
 		colly.MaxDepth(1),
-		colly.AllowedDomains(j.domain, "www."+j.domain),
+		colly.AllowedDomains(j.Domain, "www."+j.Domain),
 		colly.Async(true),
 	)
 
@@ -53,10 +55,19 @@ func (j *job) crawl() {
 	// number of go routines.
 	c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 8})
 
-	j.data = crawl(
+	log.Println("Started", j.Uuid)
+	log.Println("Crawling", j.Domain, " . . . ")
+
+	go crawl(
 		j,
 		c,
-		j.domain,
+		j.Domain,
 		make(map[string]pageInfo),
 	)
+}
+
+func (j *job) checkData() *job {
+	j.Status = <-j.StatusChannel
+	j.Data = <-j.DataChannel
+	return j
 }
