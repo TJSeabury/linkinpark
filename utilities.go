a0dff8fa08a75a6624c806e1d446ca5dde29c4f7
TJSeabury <tylerseabury@gmail.com>
2022-06-28 14:29:26 -0400

    validate domains

--- utilities.go
+++ utilities.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"net"
 	"net/http"
 	"net/url"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -23,21 +25,31 @@ func IsUrl(str string) bool {
 
 func IsHostReachable(host string) bool {
 	timeout := 1 * time.Second
-	conn, err := net.DialTimeout("tcp", host, timeout)
-	defer conn.Close()
+	conn, err := net.DialTimeout("tcp", host+":80", timeout)
 	if err != nil {
+		//conn.Close()
+		log.Println(err)
 		return false
 	}
+	conn.Close()
 	return true
 }
 
-func getDomain(uri string) string {
+func IsDomainValid(url string) bool {
+	reg := regexp.MustCompile("^(((?!\\-))(xn\\-\\-)?[a-z0-9\\-_]{0,61}[a-z0-9]{1,1}\\.)*(xn\\-\\-)?([a-z0-9\\-]{1,61}|[a-z0-9\\-]{1,30})\\.[a-z]{2,}$")
+	return reg.Match([]byte(url))
+}
+
+func getDomain(uri string) (string, error) {
 	u, err := url.Parse(uri)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 	parts := strings.Split(u.Hostname(), ".")
-	return parts[len(parts)-2] + "." + parts[len(parts)-1]
+	if len(parts)-2 < 0 || len(parts)-1 < 0 {
+		return "", errors.New("Invalid domain!")
+	}
+	return parts[len(parts)-2] + "." + parts[len(parts)-1], nil
 }
 
 func decode(r *http.Request) message {
