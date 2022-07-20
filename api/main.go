0539dc01ba76e85d329b1e0a8659e5b7428059db
Tyler Seabury <tylerseabury@gmail.com>
2022-07-20 17:02:49 -0400

    directories restructured, svelte installed

--- api/main.go
+++ api/main.go
@@ -1,21 +1,13 @@
 package main
 
 import (
-	"embed"
-	"io/fs"
 	"net/http"
 )
-
-//go:embed static
-var static embed.FS
 
 func main() {
 	addr := ":7777"
 
 	d := NewDispatcher()
-
-	contentStatic, _ := fs.Sub(static, "static")
-	http.Handle("/", http.FileServer(http.FS(contentStatic)))
 
 	http.HandleFunc("/api/start/", d.Start)
 	http.HandleFunc("/api/check/", d.Check)
