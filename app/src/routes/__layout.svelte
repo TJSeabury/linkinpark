e32b4f922c79b1cad25521e7f46604e8e9037292
Tyler Seabury <tylerseabury@gmail.com>
2022-07-26 17:07:35 -0400

    installed tailwindcss, tweaked styles

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -1,1 +1,12 @@
+<script>
+	import '../app.css';
+
+	const _app_version = '0.3.2';
+</script>
+
 <slot />
+
+<footer class="flex justify-between p-8">
+	<small>{_app_version}</small>
+	<small>&copy; Tyler Seabury 2022</small>
+</footer>
